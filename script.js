265a5f742a69946ba6d6a19d242bb8b85f7a4c28
Melissa.Kinsey <kinsey.melissa@gmail.com>
2020-12-06 13:09:52 -0600

    fix: Correct the column alignment in the flex container.

--- script.js
+++ script.js
@@ -20,6 +20,43 @@ document.getElementById("result").innerHTML = localStorage.getItem("lastname");
 var hour = $(".hour");
 var timeBlock = $(".time-block");
 var saveBtn = $(".saveBtn");
+var agenda = [];
+
+init();
+function init() {
+    // Get stored agenda from localStorage
+    // Parsing the JSON string to an object
+    var storedAgenda = JSON.parse(localStorage.getItem("agenda"));
+  
+    // If agenda was retrieved from localStorage, update the agenda array
+    if (storedAgenda !== null) {
+      agenda = storedAgenda;
+    }
+  
+    // Render todos to the DOM
+    renderAgenda();
+  }
+  
+function renderAgenda() {
+  // Clear todoList element and update todoCountSpan
+  timeBlock.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete";
+
+    li.appendChild(button);
+    todoList.appendChild(li);
+  }
+}
 
 
 
